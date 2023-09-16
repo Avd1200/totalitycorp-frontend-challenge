322ae0fe8b16acc1e20140b6a97263142add299b
Avdhesh Chaudhary <avdheshchaudhary00100@gmail.com>
2023-09-16 19:12:17 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Home from "./modules/Home";
+import { Route, Routes } from "react-router-dom";
+import Product from "./modules/Product";
+import Products from "./modules/Products";
+import CategoryProducts from "./modules/CategoryProducts";
+import Cart from "./modules/Cart";
+import Checkout from "./modules/Checkout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/categories/:name" element={<CategoryProducts/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
+        <Route path="/contact" element={<div className="h-[55vh] text-4xl flex justify-center items-center">Contact Us</div>}/>
+        <Route path="/about" element={<div className="h-[55vh] text-4xl flex justify-center items-center">About Us</div>}/>
+        <Route path="*" element={<div className="h-[55vh] text-4xl flex justify-center items-center">404!</div>} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
